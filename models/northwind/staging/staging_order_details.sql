b9ae361c1d603f4fdb458d12860553173ea9d46f
Kai Naegle <kainaegle@Kais-MacBook-Pro.local>
2026-04-20 11:43:16 +0200

    added stagoing order details

---
+++ models/northwind/staging/staging_order_details.sql
@@ -0,0 +1,11 @@
+WITH source_data AS (
+    SELECT *
+    FROM {{ source('northwind_data', 'order_details') }}
+)
+SELECT
+    orderid AS order_id,
+    productid AS product_id,
+    unitprice::NUMERIC AS unit_price,
+    quantity::INT AS quantity,
+    discount::NUMERIC AS discount
+FROM source_data
